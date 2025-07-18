d860070daa19d007fd48ed240036c8cbc6991161
loup <loup.quehec@gmail.com>
2025-07-18 17:36:16 +0200

    test

--- sync_tmdb/flows/language/sync_tmdb_language.py
+++ sync_tmdb/flows/language/sync_tmdb_language.py
@@ -108,7 +108,6 @@ def sync_tmdb_language(date: date = date.today()):
 
 		config.log_manager.success()
 	except Exception as e:
-		logger.error(f"❌ Error while syncing TMDB language for {date}: {repr(e)}")
-		# config.log_manager.failed()
+		config.log_manager.failed()
 		raise ValueError(f"Failed to sync language: {e}")
 
